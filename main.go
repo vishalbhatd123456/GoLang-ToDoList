8b7e6ae673eefb7b3505697198d697d5e6a9a38f
Vishal D Bhat <60818841+vishalbhatd123456@users.noreply.github.com>
2023-10-07 20:03:27 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,50 @@
 package main
+var addFlag string;
+var listFlag bool;
 
 import "fmt"
-func main(){
-	fmt.Println("Welcome to the To-Do List CLI!");
+
+//global var
+var tasks[] Task;
+var nextID = 1;
+
+
+type Task struct{
+	ID int;
+	Name string;
+	Done bool;
+}
+
+func addTask(name string){
+	task:=Task{
+		ID: nextID,
+		Name: name,
+		Done:false,
+	}
+	tasks= append(tasks, task)
+	nextID++;
+}
+
+func listTasks(){
+	for _, task := range tasks{
+		status := "Pending"
+		if task.Done{
+			status = "Done"
+		}
+		fmt.Printf("[%d] %s -%s\n",task.ID,task.Name,status);
+	}
+}
+
+func main() {
+	flag.StringVar(&addFlag,"add","","Add a new task");
+	flag.BoolVar(&listFlag,"list", false, "List all tasks");
+	flag.Parse();
+
+	if addFlag!=""{
+		addTask(addFlag);
+		fmt.Println("Task added");
+	}
+	if listFlag{
+		listTasks()
+	}
 }
